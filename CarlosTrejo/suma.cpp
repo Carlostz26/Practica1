b878a9bc704b49685d8c453dd59acf4589523d26
Carlos Trejo <carlos.trejo.zambrano@utelvt.edu.ec>
2022-03-25 19:35:26 -0500

    inicial

---
+++ CarlosTrejo/suma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	system(«color 17»);
+	float x,y,z;
+	cout<<"Ingrese 2 numero separados de espacio x y :";
+	cin>>x>>y;
+	c=x+y;
+	cout<<"El resultado es :";
+	cout<<z<<endl;
+	return 0;
+}
